cc365f992e10a37090e90b7d90c4ce57143a0f15
karthikeya-21 <kaarthikeyadeekonda@gmail.com>
2024-04-20 01:54:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,12 +15,13 @@ def detect():
         if video_file:
             # Call deep fake detection function from model.py
             # result = detect_deep_fake(video_file)
-            video_path = 'uploads/' + video_file.filename
+            video_path = os.path.join(app.config['UPLOAD_FOLDER'], video_file.filename)
             video_file.save(video_path)
             result = predict_video(video_path)
-            os.remove(video_path)
             return render_template('index.html', result=result)
     return render_template('index.html', error='No file uploaded')
 
 if __name__ == '__main__':
+    app.config['UPLOAD_FOLDER'] = 'static'
     app.run(debug=True)
+    
